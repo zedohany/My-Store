f2a67e22d4f1d2aa638f7aa360bfa4d5e197ad28
zedohany <ziadhany899@gmail.com>
2023-12-28 22:04:53 +0200

    add NavMain

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,3 +1,4 @@
+<!--suppress ALL -->
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
 import Welcome from '@/Components/Welcome.vue';
@@ -6,8 +7,8 @@ import Welcome from '@/Components/Welcome.vue';
 <script>
 export default {
     name: 'locale-changer',
-    data () {
-        return { langs: ['ar', 'en'] }
+    data() {
+        return {langs: ['ar', 'en']}
     }
 }
 </script>
@@ -17,13 +18,20 @@ export default {
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 Dashboard {{ $t('Dashboard') }}
+                <div class="locale-changer">
+                    <select v-model="$i18n.locale">
+                      <option v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang">
+                        {{ lang }}
+                      </option>
+                    </select>
+                  </div>
             </h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
+                    <Welcome/>
                 </div>
             </div>
         </div>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
+import NavMain from '@/Components/NavMain.vue';
 
 defineProps({
     canLogin: Boolean,
@@ -12,8 +13,10 @@ defineProps({
 <template>
     <Head title="Welcome" />
 
+    <NavMain class="mb-16" />
+
     <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-        <div v-if="canLogin" class="sm:fixed sm:top-0 sm:end-0 p-6 text-end z-10">
+        <!-- <div v-if="canLogin" class="sm:fixed sm:top-0 sm:end-0 p-6 text-end z-10">
             <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</Link>
 
             <template v-else>
@@ -21,7 +24,7 @@ defineProps({
 
                 <Link v-if="canRegister" :href="route('register')" class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</Link>
             </template>
-        </div>
+        </div> -->
 
         <div class="max-w-7xl mx-auto p-6 lg:p-8">
             <div class="flex justify-center">
